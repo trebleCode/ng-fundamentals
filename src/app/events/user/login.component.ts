a9f77098141864f49be0e7eb55b62d42ddaf5334
Scott Sweeney <treblecode@users.noreply.github.com>
2018-09-17 20:57:18 -0400

    s7_v8

--- src/app/events/user/login.component.ts
+++ src/app/events/user/login.component.ts
@@ -5,11 +5,14 @@ import { Router } from '@angular/router';
 
 @Component({
     templateUrl: 'login.component.html'
+    styles: [`
+        em { float: right; color: #E05C65; padding-left: 10px; }
+    `]
 })
 export class LoginComponent {
     username;
     password;
-
+    mouseoverLogin;
     constructor(private authService: AuthService, private router: Router) {
     }
         login(formValues) {
