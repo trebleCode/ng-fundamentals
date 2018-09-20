8670eeaa5ffbea48cfc5d6dc63b191d3da2cfffa
Scott Sweeney <treblecode@users.noreply.github.com>
2018-09-19 21:52:42 -0400

    s11_complete - using injection token, alternate provider methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { ToastrService } from './common/toastr.service';
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service';
 import { CollapsibleWellComponent } from './common/collapsible-well.component';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
@@ -22,6 +22,8 @@ import {
   SessionListComponent,
   DurationPipe
 } from './events/index';
+
+declare let toastr: Toastr;
 
 @NgModule({
   imports: [
@@ -45,7 +47,7 @@ import {
   ],
   providers: [
     EventService,
-    ToastrService,
+    { provide: TOASTR_TOKEN, useValue: toastr },
     EventRouteActivator,
     EventListResolver,
     AuthService,
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,4 @@
 import { IEvent } from './shared/event.model';
-import { ToastrService } from './../common/toastr.service';
 import { EventService } from './shared/event.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -12,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
         <hr/>
         <div class="row">
             <div *ngFor="let event of events" class="col-md-5">
-                <event-thumbnail (click)=handleThumbnailClick(event.name) [event]="event"></event-thumbnail>
+                <event-thumbnail [event]="event"></event-thumbnail>
             </div>
         </div>
     </div>
@@ -23,16 +22,12 @@ export class EventsListComponent implements OnInit {
 
     events: IEvent[];
 // tslint:disable-next-line:eofline
-    constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {
+    constructor(private eventService: EventService, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
         // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         // Add 'implements OnInit' to the class.
         this.events = this.route.snapshot.data['events'];
-    }
-
-    handleThumbnailClick(eventName) {
-        this.toastr.success(eventName);
     }
 }
